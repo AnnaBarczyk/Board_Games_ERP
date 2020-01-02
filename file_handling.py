be2c658bbaeecdd8fc33a39a00c14397ce78e8f6
Anna Barczyk <anna.ewa.barczyk@gmail.com>
2020-01-03 00:07:14 +0100

    File export added in file_handling.py

--- file_handling.py
+++ file_handling.py
@@ -1,13 +1,15 @@
 # Obsługa plików
 
+import ui
+
 def import_file(filename="games.csv"):
     '''
     Importowanie danych z pliku .csv, zwracanie listy list. Parametr: nazwa pliku.
     Docelowy format:
-    [['Dixit', ' Rebel', ' 2010', ' 3', ' 6', ' imprezowa', ' 30', ' brak'], 
-    ['List Miłosny', ' 2013', ' 2', ' 5', ' imprezowa', ' 20', ' brak'], 
-    ['Pentago', ' Egmont', ' 2017', ' 2', ' 2', ' logiczna', ' 15', ' brak']]
+    [['Dixit', 'Rebel', '2010', '3', '6', 'imprezowa', '30', 'brak'], 
+    ['List Miłosny', '2013', '2', '5', 'imprezowa', '20', 'brak']]
     '''
+
     games_list_of_lists = []
 
     with open(filename, "r") as file:
@@ -16,12 +18,17 @@ def import_file(filename="games.csv"):
     return games_list_of_lists
 
 
-def export_file(games, filename="games.scv", mode="a"):
+def export_file(games_list_of_lists, filename="games.scv", mode="a"):
     '''
     Exortowanie danych do pliku .csv, zwracanie listy list. 
     Parametry: lista nowych gier (z game reports), nazwa pliku docelowego, 
     tryb zapisu "a" dodaje wpisy, tryb "w" nadpisuje plik .
     '''
+    if mode not in ['a','w']:
+        ui.print_error_msg('Wrong write mode')
+    with open(filename, mode) as file:
+        for game in games_list_of_lists:
+            file.write(';'.join(line) + '\r\n')
 
 ## Opcjonalnie
 
--- ui.py
+++ ui.py
@@ -2,23 +2,24 @@
 
 
 def print_menu(commands):
-'''
-Przedstawia menu przyjazne dla użytkownika. Paramter: commands (lista).
-'''
+    '''
+    Przedstawia menu przyjazne dla użytkownika. Paramter: commands (lista).
+    '''
 
 def print_table(games_list_of_lists):
-'''
-Przedstawia listę list jako tabelę. Parametr: games (lista list).
-'''
+    '''
+    Przedstawia listę list jako tabelę. Parametr: games (lista list).
+    '''
 
 def get_input(ask_for, labels):
-'''
-Pobiera informacje od użytkownika i przekazuje do programu jako listę. 
-Parametry: ask for - informacja do użytkowanika (str), labels (list) - wpisywane dane w odpowiedniej kolejności
-'''
+    '''
+    Pobiera informacje od użytkownika i przekazuje do programu jako listę. 
+    Parametry: ask for - informacja do użytkowanika (str), labels (list) - wpisywane dane w odpowiedniej kolejności
+    '''
 
 def print_error_msg(message):
-'''
-Przedstawia użytkownikowi komunikat błędu. Parametr: Message (str).
-'''
+    '''
+    Przedstawia użytkownikowi komunikat błędu. Parametr: Message (str).
+    '''
+    print(message)
 
